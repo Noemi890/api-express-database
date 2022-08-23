d366f6c5f6a7b837548278b55088d69c3a97b803
Noemi <noemi.caggiano@gmail.com>
2022-08-23 17:05:56 +0100

    get all books with query params

--- src/routers/books.js
+++ src/routers/books.js
@@ -3,7 +3,27 @@ const router = express.Router()
 const db = require("../../db");
 
 router.get('/', async (req, res) => {
+  const type = req.query.type
+  let SQLMainQuery = 'SELECT * FROM books'
+  const params = []
 
+  if (type) {
+    SQLMainQuery += ' WHERE type = $1'
+    params.push(type)
+  }
+
+  console.log(SQLMainQuery)
+
+  const queryResult = await db.query(SQLMainQuery, params)
+  console.log('my result', queryResult)
+
+  res.json({
+    books: queryResult.rows
+  })
+})
+
+router.post('/', async (req, res) => {
+  
 })
 
 module.exports = router
